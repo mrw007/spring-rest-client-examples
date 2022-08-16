b03d9b9202674c64a2653738b3192ca3aa92a0af
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-16 15:01:32 +0100

    Added web form and view

---
+++ src/test/java/mrw007/springframework/springrestclientexamples/controllers/UserControllerTest.java
@@ -0,0 +1,54 @@
+package mrw007.springframework.springrestclientexamples.controllers;
+
+import mrw007.springframework.springrestclientexamples.services.ApiService;
+import mrw007.springframework.springrestclientexamples.services.ApiServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = UserController.class)
+@Import({ApiServiceImpl.class, RestTemplate.class})
+class UserControllerTest {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void index() {
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Disabled
+    @Test
+    void formPost() {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("limit", "3");
+
+        webTestClient.post().uri("/users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
