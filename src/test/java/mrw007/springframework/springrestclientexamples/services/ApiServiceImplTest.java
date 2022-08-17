baa3c40ce83adf3dd0d0dd9fa5335682848be3ae
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-17 12:03:31 +0100

    Fixed Service test with reactive tests

--- src/test/java/mrw007/springframework/springrestclientexamples/services/ApiServiceImplTest.java
+++ src/test/java/mrw007/springframework/springrestclientexamples/services/ApiServiceImplTest.java
@@ -3,26 +3,42 @@ package mrw007.springframework.springrestclientexamples.services;
 import mrw007.springframework.api.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+@Import({ApiServiceImpl.class, RestTemplate.class})
 class ApiServiceImplTest {
 
+
+    @Autowired
     ApiService apiService;
 
     @BeforeEach
     void setUp() {
-        apiService = new ApiServiceImpl(new RestTemplate());
     }
 
     @Test
     void getUsers() {
-        List<User> users = apiService.getUsers(3);
-        assertEquals(3, users.size());
+        Flux<User> users = apiService.getUsers(Mono.just(3));
+        assertNotNull(users);
+
+        StepVerifier.create(users)
+                .recordWith(ArrayList::new)
+                .thenConsumeWhile(x -> true)
+                .expectRecordedMatches(elements -> elements.size() == 3)
+                .verifyComplete();
     }
 }
